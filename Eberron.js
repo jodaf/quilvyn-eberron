e6502ececf661c34b63ac3bd6204e4adb39b56b9
Jim Hayes <spinaltap12345@gmail.com>
2020-05-11 15:26:56 -0700

    Bump to version 1.8.

--- Eberron.js
+++ Eberron.js
@@ -17,7 +17,7 @@ Place, Suite 330, Boston, MA 02111-1307 USA.
 
 "use strict";
 
-var EBERRON_VERSION = '1.7.1.4';
+var EBERRON_VERSION = '1.8.1.0';
 
 /*
  * This module loads the rules from the Eberron campaign setting.  The Eberron
